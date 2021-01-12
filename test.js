6c891b13688d209fbf766f44d329f2ec71a29730
shrikanth <smalawad07@gmail.com>
2021-01-12 13:52:56 +0530

    added banana project, blogs, favicon image.

---
+++ test.js
@@ -0,0 +1,39 @@
+
+    fig1.
+
+    let add = function(x,y) {
+        return x + y;
+    }
+    console.log(add(5,2)); // 7;
+
+    fig2.
+
+    let add = (x,y) => x + y;
+    console.log(add(5,2)); // 7;
+
+    fig3
+
+    let multiply = (x, y) => { return x * y; };
+    console.log(multiply(3,4));
+
+
+    fig4:
+
+    (p1, p2, ..., pn) => expression;
+
+    fig5
+
+    => expression
+
+    fig6
+
+    => { return expression; }
+
+    fig5
+
+    let names = ['John', 'Mac', 'Peter'];
+    let lengths = names.map(name => name.length);
+
+    console.log(lengths);
+
+

da4fc6927bf135ba4afd1e424151f14adfdd30f5
shrikanth <smalawad07@gmail.com>
2021-01-12 20:32:33 +0530

    code cleanup and indentaion

--- test.js
+++ test.js
@@ -1,39 +1,40 @@
+fig1.
 
-    fig1.
+let add = function (x, y) {
+    return x + y;
+}
+console.log(add(5, 2)); // 7;
 
-    let add = function(x,y) {
-        return x + y;
-    }
-    console.log(add(5,2)); // 7;
+fig2.
 
-    fig2.
+let add = (x, y) => x + y;
+console.log(add(5, 2)); // 7;
 
-    let add = (x,y) => x + y;
-    console.log(add(5,2)); // 7;
+fig3
 
-    fig3
-
-    let multiply = (x, y) => { return x * y; };
-    console.log(multiply(3,4));
+let multiply = (x, y) => {
+    return x * y;
+};
+console.log(multiply(3, 4));
 
 
-    fig4:
+fig4:
 
     (p1, p2, ..., pn) => expression;
 
-    fig5
+fig5
 
     => expression
 
-    fig6
+fig6
 
-    => { return expression; }
+    => {
+        return expression;
+    }
 
-    fig5
+fig5
 
-    let names = ['John', 'Mac', 'Peter'];
-    let lengths = names.map(name => name.length);
+let names = ['John', 'Mac', 'Peter'];
+let lengths = names.map(name => name.length);
 
-    console.log(lengths);
-
-
+console.log(lengths);
